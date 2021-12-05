ed749989e5ea7f76f964706c319b9ea0892d5592
Ryan Hagan <ryan.hagan@modea.com>
2021-12-05 18:33:21 -0500

    Added Day 6 scaffolding

---
+++ Day06/Part1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+
+namespace AdventOfCode2021.Day06
+{
+    public class Part1
+    {
+        public void Run(string[] data)
+        {
+        }
+    }
+}
---
+++ Day06/tests/Day06Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+using AdventOfCode2021.Day06;
+using Xunit;
+
+namespace AdventOfCode2021.Day06.Tests
+{
+    public class Day06Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
