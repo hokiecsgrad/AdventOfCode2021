a2a122ff721258ba21904b6c4e5b87e70763d0b8
Ryan Hagan <ryan@ryanhagan.net>
2021-12-06 12:53:30 -0500

    Solved Part 1 of Day 6

--- Day06/Part1.cs
+++ Day06/Part1.cs
@@ -9,6 +9,23 @@ namespace AdventOfCode2021.Day06
     {
         public void Run(string[] data)
         {
+            List<int> ages = data[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            for (int i = 0; i < 80; i++)
+            {
+                for (int j = ages.Count() - 1; j >= 0; j--)
+                {
+                    if (ages[j] == 0)
+                    {
+                        ages[j] = 6;
+                        ages.Add(8);
+                    }
+                    else
+                        ages[j] -= 1;
+                }
+            }
+
+            System.Console.WriteLine($"The number of fish after 80 days is: {ages.Count()}");
         }
     }
 }
--- Day06/tests/Day06Tests.cs
+++ Day06/tests/Day06Tests.cs
@@ -9,10 +9,31 @@ namespace AdventOfCode2021.Day06.Tests
 {
     public class Day06Tests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(18, 26)]
+        [InlineData(80, 5934)]
+        public void Part1_WithSampleData_ShouldReturnExpectedLengthAfterSetNumberOfDays(int numDays, int expectedFish)
         {
+            string[] data = SampleData.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            List<int> ages = data[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
 
+            for (int i = 0; i < numDays; i++)
+            {
+                for (int j = ages.Count() - 1; j >= 0; j--)
+                {
+                    if (ages[j] == 0)
+                    {
+                        ages[j] = 6;
+                        ages.Add(8);
+                    }
+                    else
+                        ages[j] -= 1;
+                }
+            }
+
+            Assert.Equal(expectedFish, ages.Count);
         }
+
+        private string SampleData = @"3,4,3,1,2";
     }
 }
