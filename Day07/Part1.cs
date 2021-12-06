230fb596e3f68785e84d373a1d9c2cc27dcd0a86
Ryan Hagan <ryan@ryanhagan.net>
2021-12-06 17:58:34 -0500

    Day 7 scaffolding

---
+++ Day07/Part1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+
+namespace AdventOfCode2021.Day07
+{
+    public class Part1
+    {
+        public void Run(string[] data)
+        {
+        }
+    }
+}
---
+++ Day07/tests/Day07Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+using AdventOfCode2021.Day07;
+using Xunit;
+
+namespace AdventOfCode2021.Day07.Tests
+{
+    public class Day07Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
