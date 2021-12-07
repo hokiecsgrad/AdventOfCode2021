41da8cae3db6426be6558918f63b3bf0531fb864
Ryan Hagan <ryan@ryanhagan.net>
2021-12-07 12:48:52 -0500

    Solved Day 7

--- Day07/Part2.cs
+++ Day07/Part2.cs
@@ -9,6 +9,26 @@ namespace AdventOfCode2021.Day07
     {
         public void Run(string[] data)
         {
+            Dictionary<int, int> positions = data[0].Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .GroupBy(x => x)
+                    .Select(x => new { Pos = int.Parse(x.Key), NumSubs = x.Count() })
+                    .ToDictionary(group => group.Pos, group => group.NumSubs);
+
+            int maxPositions = positions.Keys.Max();
+            int fuleNeeded = int.MaxValue;
+            for (int posIndex = 0; posIndex < maxPositions; posIndex++)
+            {
+                int fuleNeededToMoveAllSubsToCurrPos = 0;
+                foreach (var posInfo in positions)
+                    fuleNeededToMoveAllSubsToCurrPos += SumOfNumbers(Math.Abs(posIndex - posInfo.Key)) * posInfo.Value;
+
+                fuleNeeded = Math.Min(fuleNeeded, fuleNeededToMoveAllSubsToCurrPos);
+            }
+
+            System.Console.WriteLine($"The minimum amount of fule needed to align all crab subs is: {fuleNeeded}");
         }
+
+        private int SumOfNumbers(int num)
+            => Enumerable.Range(1, num).Sum();
     }
 }
--- Day07/tests/Day07Tests.cs
+++ Day07/tests/Day07Tests.cs
@@ -10,9 +10,52 @@ namespace AdventOfCode2021.Day07.Tests
     public class Day07Tests
     {
         [Fact]
-        public void Test1()
+        public void Part1_WithSampleData_ShouldReturn37()
         {
+            Dictionary<int, int> positions = SampleData.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .GroupBy(x => x)
+                    .Select(x => new { Pos = int.Parse(x.Key), NumSubs = x.Count() })
+                    .ToDictionary(group => group.Pos, group => group.NumSubs);
 
+            int maxPositions = positions.Keys.Max();
+            int fuleNeeded = int.MaxValue;
+            for (int posIndex = 0; posIndex < maxPositions; posIndex++)
+            {
+                int fuleNeededToMoveAllSubsToCurrPos = 0;
+                foreach (var posInfo in positions)
+                    fuleNeededToMoveAllSubsToCurrPos += Math.Abs((posIndex - posInfo.Key) * posInfo.Value);
+
+                fuleNeeded = Math.Min(fuleNeeded, fuleNeededToMoveAllSubsToCurrPos);
+            }
+
+            Assert.Equal(37, fuleNeeded);
         }
+
+        [Fact]
+        public void Part2_WithSampleData_ShouldReturn168()
+        {
+            Dictionary<int, int> positions = SampleData.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .GroupBy(x => x)
+                    .Select(x => new { Pos = int.Parse(x.Key), NumSubs = x.Count() })
+                    .ToDictionary(group => group.Pos, group => group.NumSubs);
+
+            int maxPositions = positions.Keys.Max();
+            int fuleNeeded = int.MaxValue;
+            for (int posIndex = 0; posIndex < maxPositions; posIndex++)
+            {
+                int fuleNeededToMoveAllSubsToCurrPos = 0;
+                foreach (var posInfo in positions)
+                    fuleNeededToMoveAllSubsToCurrPos += SumOfNumbers(Math.Abs(posIndex - posInfo.Key)) * posInfo.Value;
+
+                fuleNeeded = Math.Min(fuleNeeded, fuleNeededToMoveAllSubsToCurrPos);
+            }
+
+            Assert.Equal(168, fuleNeeded);
+        }
+
+        private int SumOfNumbers(int num)
+            => Enumerable.Range(1, num).Sum();
+
+        private string SampleData = @"16,1,2,0,4,2,7,1,2,14";
     }
 }
