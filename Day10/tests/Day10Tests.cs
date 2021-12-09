9d92b095113f2d46e4c816934d22e285acf997d5
Ryan Hagan <ryan.hagan@modea.com>
2021-12-09 16:10:14 -0500

    Day 10 scaffolding

---
+++ Day10/tests/Day10Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+using AdventOfCode2021.Day10;
+using Xunit;
+
+namespace AdventOfCode2021.Day10.Tests
+{
+    public class Day10Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
