e2ae82ff5dc7fd8813255ae918f1168275bce166
Ryan Hagan <ryan@ryanhagan.net>
2021-12-08 22:24:07 -0500

    Day 9 scaffolding

---
+++ Day09/tests/Day09Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+using AdventOfCode2021.Day09;
+using Xunit;
+
+namespace AdventOfCode2021.Day09.Tests
+{
+    public class Day09Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
