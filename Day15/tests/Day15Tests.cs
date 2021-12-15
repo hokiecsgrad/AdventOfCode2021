d5770999d334c8a26102327bc6c037d9ba5a7f72
Ryan Hagan <ryan.hagan@modea.com>
2021-12-15 10:37:20 -0500

    Added Day 15 scaffolding

---
+++ Day15/tests/Day15Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+using AdventOfCode2021.Day15;
+using Xunit;
+
+namespace AdventOfCode2021.Day15.Tests
+{
+    public class Day15Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
