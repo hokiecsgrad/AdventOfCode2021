f27de0add7de3c29fb7a80dd4e69da4e60ca90cf
Ryan Hagan <ryan@ryanhagan.net>
2021-12-17 21:14:44 -0500

    Day 16 scaffolding

---
+++ Day16/tests/Day16Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+using AdventOfCode2021.Day16;
+using Xunit;
+
+namespace AdventOfCode2021.Day16.Tests
+{
+    public class Day16Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
